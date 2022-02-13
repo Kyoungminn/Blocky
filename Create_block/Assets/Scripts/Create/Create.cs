22a04f4c1cd428b68f402dbf6961db22bd87a62c
Juyie <ghkdwndl1004@ewhain.net>
2022-02-13 14:01:30 +0900

    add: create block by keyboard 'G'

--- Create_block/Assets/Scripts/Create/Create.cs
+++ Create_block/Assets/Scripts/Create/Create.cs
@@ -51,12 +51,14 @@ public class Create : MonoBehaviour
         // 현재 컨트롤러가 향하는 방향에 아무것도 없고, 생성이 완료되지 않은 블록이 없는 경우에 생성 가능
         if (!isRayHit && GameObject.FindGameObjectsWithTag("Test").Length == 0)
         {
-            if (rightTriggerReference.action.ReadValue<float>() > 0.0f || leftTriggerReference.action.ReadValue<float>() > 0.0f) //트리거 버튼 누르면 생성되도록(원래 트리거 버튼은 왼쪽 마우스이지만 텍스트나 버튼 입력과 혼동되서 g키를 누르면 생성되도록 Unity에는 grip으로 지정해놓았습니다)
+            if (rightTriggerReference.action.ReadValue<float>() > 0.0f || leftTriggerReference.action.ReadValue<float>() > 0.0f || Input.GetKeyDown(KeyCode.G)) //트리거 버튼 누르면 생성되도록(원래 트리거 버튼은 왼쪽 마우스이지만 텍스트나 버튼 입력과 혼동되서 g키를 누르면 생성되도록 Unity에는 grip으로 지정해놓았습니다)
             {
                 if (rightTriggerReference.action.ReadValue<float>() > 0.0f)
                     rayPos = rightFront.GetComponent<Transform>(); //컨트롤러의 위치 갖고 옴
-                else if(leftTriggerReference.action.ReadValue<float>() > 0.0f)
+                else if (leftTriggerReference.action.ReadValue<float>() > 0.0f)
                     rayPos = leftFront.GetComponent<Transform>(); //컨트롤러의 위치 갖고 옴
+                else
+                    rayPos = rightFront.GetComponent<Transform>();
                 canvas.transform.position = new Vector3(rayPos.position.x, rayPos.position.y + 2, rayPos.position.z + 2); //컨트롤러 위치에서 z좌표만 +100
                 canvas.SetActive(true);
 
