748c192264965cd584cdb5f9817cd4924142ba99
Juyie <ghkdwndl1004@ewhain.net>
2022-03-18 17:50:55 +0900

    fix: fix load files

--- Create_block/Assets/Scripts/SaveLoad/LoadFile.cs
+++ Create_block/Assets/Scripts/SaveLoad/LoadFile.cs
@@ -12,7 +12,7 @@ public class LoadFile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadFiles();
     }
 
     public void LoadFiles()
