7753fa89f9f31aeccc065c231abd341c9d467ac2
Juyie <ghkdwndl1004@ewhain.net>
2022-05-13 17:55:30 +0900

    add: add tag networking script

---
+++ Create_block/Assets/Scripts/Multimode/TagNetworking.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TagNetworking : MonoBehaviour
+{
+    private string currentTag;
+    private string tempTag;
+    private PhotonView photonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        currentTag = gameObject.tag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.tag != currentTag)
+            currentTag = gameObject.tag;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(currentTag);
+        }
+        else
+        {
+            tempTag = (string)stream.ReceiveNext();
+            gameObject.tag = tempTag;
+        }
+    }
+}
