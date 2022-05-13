86f76ea62b85be03281c4b479d222fd81afb03db
Juyie <ghkdwndl1004@ewhain.net>
2022-05-13 17:59:56 +0900

    fix: fix tag networking script

--- Create_block/Assets/Scripts/Multimode/TagNetworking.cs
+++ Create_block/Assets/Scripts/Multimode/TagNetworking.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class TagNetworking : MonoBehaviour
+public class TagNetworking : MonoBehaviour, IPunObservable
 {
     private string currentTag;
     private string tempTag;
